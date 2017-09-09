8e9fefce8789f0aac73743dce31369d563c9003f
leichangchun <l495735915@163.com>
2017-09-09 15:58:40 +0800

    增加了css loader的include配置，不然 import css文件时会报错

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -80,7 +80,17 @@ module.exports = {
             loader: "css-loader" // translates CSS into CommonJS
         }, {
             loader: "sass-loader" // compiles Sass to CSS
-        }]
+        }],
+        include : []
+      },
+      {
+        test: /\.css$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }],
+        include : []
       }
     ]
   }
