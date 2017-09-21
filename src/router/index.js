f911b8f9bfeac3d69f71bb6798f2fc77191f7457
leichangchun <l495735915@163.com>
2017-09-21 20:59:26 +0800

    增加 新增主题组件的路由 mate

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const topic = () => import('../components/topic.vue')
 const login = () => import('../components/login.vue')
 const detail = () => import('../components/detail.vue')
 const about = () => import('../components/about.vue')
+const add = () => import('../components/add.vue')
 Vue.use(Router)
 
 export default new Router({
@@ -31,6 +32,13 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: about
+    }, {
+      path: '/add',
+      name: 'add',
+      component: add,
+      meta: {     // meta是路由记录字段 可以自定义字段  此处定义了一个标志需要登录的字段
+        reqireLogin: true
+      }
     }
   ]
 })
